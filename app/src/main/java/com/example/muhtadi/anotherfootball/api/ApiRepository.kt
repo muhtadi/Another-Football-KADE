4615ac9a2d0c362c3900803f5354e7e8eb7ed304
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-11-08 19:46:16 +0700

    adding api repository

---
+++ app/src/main/java/com/example/muhtadi/anotherfootball/api/ApiRepository.kt
@@ -0,0 +1,10 @@
+package com.example.muhtadi.anotherfootball.api
+
+import java.net.URL
+
+class ApiRepository {
+
+    fun doRequest(url: String): String {
+        return URL(url).readText()
+    }
+}
